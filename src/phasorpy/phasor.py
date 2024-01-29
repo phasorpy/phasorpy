97563100e279b2888a4adc6e4517ea414b5f21e6
bruno-pannunzio <brunopannunzio@gmail.com>
2024-01-29 19:58:08 -0300

    Update docstring example

--- src/phasorpy/phasor.py
+++ src/phasorpy/phasor.py
@@ -276,7 +276,7 @@ def phasor_to_polar(
 
     Examples
     --------
-    >>> phase, modulation = phasor_to_polar(
+    >>> phasor_to_polar(
     ...     [1.0, 2.0, 3.0], [4.0, 5.0, 6.0]
     ... )
     (array(...), array(...))
