18c2a3601da2beb0d876aab6ca054f43ef75c43d
Christoph Gohlke <cgohlke@cgohlke.com>
2024-03-05 10:01:41 -0800

    Add plot module (#36)

---
+++ tests/test__utils.py
@@ -0,0 +1,134 @@
+"""Test the phasorpy._utils module."""
+
+import math
+
+import pytest
+from numpy.testing import assert_allclose
+
+from phasorpy._utils import (
+    circle_circle_intersection,
+    circle_line_intersection,
+    kwargs_notnone,
+    parse_kwargs,
+    phasor_from_polar_scalar,
+    phasor_to_polar_scalar,
+    scale_matrix,
+    sort_coordinates,
+    update_kwargs,
+)
+
+
+def test_phasor_to_polar_scalar():
+    """Test phasor_to_polar_scalar function."""
+    assert phasor_to_polar_scalar(0.0, 0.0) == (0.0, 0.0)
+    assert_allclose(
+        phasor_to_polar_scalar(0.8, 0.4, degree=True, percent=True),
+        (26.565051, 89.442719),
+        atol=1e-6,
+    )
+    assert_allclose(
+        phasor_to_polar_scalar(1.0, 0.0, degree=True, percent=True),
+        (0.0, 100.0),
+        atol=1e-6,
+    )
+
+
+def test_phasor_from_polar_scalar():
+    """Test phasor_from_polar_scalar function."""
+    assert phasor_from_polar_scalar(0.0, 0.0) == (0.0, 0.0)
+    assert_allclose(
+        phasor_from_polar_scalar(
+            26.565051, 89.442719, degree=True, percent=True
+        ),
+        (0.8, 0.4),
+        atol=1e-6,
+    )
+    assert_allclose(
+        phasor_from_polar_scalar(0.0, 100.0, degree=True, percent=True),
+        (1.0, 0.0),
+        atol=1e-6,
+    )
+    # roundtrip
+    assert_allclose(
+        phasor_from_polar_scalar(
+            *phasor_to_polar_scalar(-0.4, -0.2, degree=True, percent=True),
+            degree=True,
+            percent=True,
+        ),
+        (-0.4, -0.2),
+        atol=1e-6,
+    )
+
+
+def test_parse_kwargs():
+    """Test parse_kwargs function."""
+    kwargs = {'one': 1, 'two': 2, 'four': 4}
+    kwargs2 = parse_kwargs(kwargs, 'two', 'three', four=None, five=5)
+    assert kwargs == {'one': 1}
+    assert kwargs2 == {'two': 2, 'four': 4, 'five': 5}
+
+    kwargs = {'one': 1, 'two': 2, 'four': 4}
+    kwargs2 = parse_kwargs(
+        kwargs, 'two', 'three', four=None, five=5, _del=False
+    )
+    assert kwargs == {'one': 1, 'two': 2, 'four': 4}
+    assert kwargs2 == {'two': 2, 'four': 4, 'five': 5}
+
+
+def test_update_kwargs():
+    """Test update_kwargs function."""
+    kwargs = {
+        'one': 1,
+    }
+    update_kwargs(kwargs, one=None, two=2)
+    assert kwargs == {'one': 1, 'two': 2}
+
+
+def test_kwargs_notnone():
+    """Test kwargs_notnone function."""
+    assert kwargs_notnone(one=1, none=None) == {'one': 1}
+
+
+def test_scale_matrix():
+    """Test scale_matrix function."""
+    assert_allclose(
+        scale_matrix(1.1, (0.0, 0.5)),
+        [[1.1, 0, -0], [0, 1.1, -0.05], [0, 0, 1]],
+        1e-6,
+    )
+
+
+def test_sort_coordinates():
+    """Test sort_coordinates function."""
+    x, y = sort_coordinates([0, 1, 2, 3], [0, 1, -1, 0])
+    assert_allclose(x, [2, 3, 1, 0])
+    assert_allclose(y, [-1, 0, 1, 0])
+
+    x, y = sort_coordinates([0, 1, 2], [0, 1, -1])
+    assert_allclose(x, [0, 1, 2])
+    assert_allclose(y, [0, 1, -1])
+
+    with pytest.raises(ValueError):
+        sort_coordinates([0, 1, 2, 3], [0, 1, -1])
+
+
+def test_circle_line_intersection():
+    """Test circle_line_intersection function."""
+    assert_allclose(
+        circle_circle_intersection(
+            0.0, 0.0, math.hypot(0.6, 0.4), 0.6, 0.4, 0.2
+        ),
+        ((0.6868, 0.2198), (0.4670, 0.5494)),
+        1e-3,
+    )
+    assert not circle_circle_intersection(0.0, 0.0, 1.0, 0.6, 0.4, 0.2)
+
+
+def test_circle_circle_intersection():
+    """Test circle_circle_intersection function."""
+    assert_allclose(
+        circle_line_intersection(0.6, 0.4, 0.2, 0.0, 0.0, 0.6, 0.4),
+        ((0.7664, 0.5109), (0.4335, 0.2890)),
+        1e-3,
+    )
+    assert not circle_line_intersection(0.6, 0.4, 0.2, 0.0, 0.0, 0.6, 0.1)
--- tutorials/phasorpy_introduction.py
+++ tutorials/phasorpy_introduction.py
@@ -1,8 +1,11 @@
 """
-Introduction
-============
+Introduction to PhasorPy
+========================
 
-This tutorial provides an introduction to using the PhasorPy library.
+An introduction to using the PhasorPy library.
+
+PhasorPy is an open-source Python library for the analysis of fluorescence
+lifetime and hyperspectral images using the :doc:`/phasor_approach`.
 
 """
 
@@ -22,53 +25,189 @@ This tutorial provides an introduction to using the PhasorPy library.
 # Install PhasorPy
 # ----------------
 #
-# On a command prompt, shell or terminal, run ``pip`` to download and install
-# the PhasorPy library and all its dependencies from the Python Package Index::
+# To download and install the PhasorPy library and all its dependencies from
+# the `Python Package Index <https://pypi.org/project/phasorpy/>`_ (PyPI),
+# run the following command on a command prompt, shell or terminal::
 #
-# $ python -m pip install -U phasorpy
+#     python -m pip install -U "phasorpy[all]"
+#
+# .. note::
+#    The PhasorPy library is in its early stages of development
+#    and has not yet been released to PyPI.
+#    The development version of PhasorPy can be `installed manually
+#    <https://packaging.python.org/en/latest/tutorials/installing-packages/>`_,
+#    for example, using the binary wheels from `GitHub Actions
+#    <https://github.com/phasorpy/phasorpy/actions/workflows/build_wheels.yml>`_,
+#    or the source code on GitHub (requires a C compiler)::
+#
+#        python -m pip install git+https://github.com/phasorpy/phasorpy.git
 
 # %%
 # Import phasorpy
 # ---------------
 #
-# Open the Python interpreter, import the ``phasorpy`` package, and check its
-# version:
+# Start the Python interpreter, import the ``phasorpy`` package,
+# and print its version:
 
 import phasorpy
 
 print(phasorpy.__version__)
 
 # %%
-# Read data file
-# --------------
+# Read signal from file
+# ---------------------
 #
-# PhasorPy provides many functions to read image and metadata from file formats
-# used in microscopy.
-# For example, ...:
+# The :py:mod:`phasorpy.datasets` module provides access to various sample
+# files, for example, a TIFF file containing a time-correlated
+# single photon counting (TCSPC) histogram obtained at 80 MHz.
+#
+# The :py:mod:`phasorpy.io` module provides many functions to read
+# time-resolved and hyperspectral image and metadata from file formats used
+# in microscopy. However, here the
+# `tifffile <https://pypi.org/project/tifffile/>`_ library is used directly:
 
-...
+# TODO: use phasorpy.io function to read histogram and metadata from PTU file
+
+import tifffile
+
+from phasorpy.datasets import fetch
+
+signal = tifffile.imread(fetch('Embryo.tif'))
+frequency = 80.0
+
+print(signal.shape, signal.dtype)
+
+# %%
+# Plot the spatial and histogram averages:
+
+from phasorpy.plot import plot_signal_image
+
+plot_signal_image(signal, axis=0)
 
 # %%
 # Calculate phasor coordinates
 # ----------------------------
 #
-# Calculate the phasor coordinates for the image stack using known reference
-# coordinates:
+# The :py:mod:`phasorpy.phasor` module provides functions to calculate,
+# convert, and correct phasor coordinates.
+#
+# Phasor coordinate are calculated from the signal, a TCSPC histogram in
+# this case. The histogram samples are in the first dimension (`axis=0`):
 
-...
+from phasorpy.phasor import phasor_from_signal
 
-# %%
-# Display phasor plot
-# -------------------
-
-from matplotlib import pyplot
-
-pyplot.imshow([[0, 1], [2, 3]])
+mean, real, imag = phasor_from_signal(signal, axis=0)
 
 # %%
-# Segment intensity image
+# Plot the calculated phasor coordinates:
+
+from phasorpy.plot import plot_phasor_image
+
+plot_phasor_image(mean, real, imag)
+
+# %%
+# Calibrate phasor coordinates
+# ----------------------------
+#
+# Phasor coordinates from time-resolved measurements must be calibrated
+# with the polar coordinates (phase and modulation) obtained from a
+# reference standard of known lifetime, acquired with the same instrument
+# settings.
+#
+# Read the signal of the reference measurement from a file:
+
+reference_signal = tifffile.imread(fetch('Fluorescein_Embryo.tif'))
+
+# %%
+# Calculate phasor coordinates from the measured reference signal:
+
+_, measured_real, measured_imag = phasor_from_signal(reference_signal, axis=0)
+
+# %%
+# Average the phasor coordinates, assuming there are no spatial aberations:
+
+from phasorpy.phasor import phasor_center
+
+measured_real, measured_imag = phasor_center(measured_real, measured_imag)
+
+# %%
+# Calculate absolute phasor coordinates from the known lifetime of the
+# reference (Fluorescein, 4.2 ns):
+
+from phasorpy.phasor import phasor_from_lifetime
+
+known_real, known_imag = phasor_from_lifetime(frequency, 4.2)
+
+# %%
+# Calculate polar coordinates (phase shift and relative modulation) to
+# correct phasor coordinates:
+
+from phasorpy.phasor import polar_from_reference_phasor
+
+phase0, modulation0 = polar_from_reference_phasor(
+    measured_real, measured_imag, known_real, known_imag
+)
+
+# %%
+# Finally, calibrate the raw phasor coordinates obtained from the signal:
+
+from phasorpy.phasor import phasor_calibrate
+
+real, imag = phasor_calibrate(real, imag, phase0, modulation0)
+
+# %%
+# Filter phasor coordinates
+# -------------------------
+#
+# Applying median filter to the calibrated phasor coordinates,
+# often multiple times, improves contrast:
+
+# TODO: replace this with a ``phasor_filter`` function?
+from skimage.filters import median
+
+for _ in range(2):
+    real = median(real)
+    imag = median(imag)
+
+# %%
+# Pixels with low intensities are commonly excluded from analysis and
+# visualization of phasor coordinates:
+
+# TODO: replace this with a ``phasor_mask`` function?
+mask = mean > 1
+real = real[mask]
+imag = imag[mask]
+
+# %%
+# Plot phasor coordinates
 # -----------------------
-# Show the intensity image, colored according to a selection of phasor
-# coordinates:
+#
+# The :py:mod:`phasorpy.plot` module provides functions and classes for
+# plotting phasor and polar coordinates.
+#
+# Large number of phasor coordinates, such as obtained from imaging,
+# are commonly visualized as 2D histograms:
 
-...
+from phasorpy.plot import PhasorPlot
+
+plot = phasorpy.plot.PhasorPlot(
+    frequency=frequency, title='Calibrated, filtered phasor coordinates'
+)
+plot.hist2d(real, imag)
+plot.show()
+
+# %%
+# For comparison, the uncalibrated, unfiltered phasor coordinates:
+
+plot = PhasorPlot(allquadrants=True, title='Raw phasor coordinates')
+plot.hist2d(*phasor_from_signal(signal, axis=0)[1:])
+plot.semicircle()
+plot.show()
+
+# %%
+# To be continued
+# ---------------
+#
+
+# %%
+# sphinx_gallery_thumbnail_number = 3
