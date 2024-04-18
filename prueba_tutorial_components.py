24f161cbbd7cb883320207f139d08dd22657fd85
bruno-pannunzio <brunopannunzio@gmail.com>
2024-04-18 09:09:31 -0300

    update fraction function

--- prueba_tutorial_components.py
+++ prueba_tutorial_components.py
@@ -1,8 +1,8 @@
 #%%
 import numpy
-from phasorpy.components import fractional_intensities_from_phasor, fractional_intensities_from_phasor_old
+from phasorpy.components import fractional_intensities_from_phasor, fractional_intensities_from_phasor_old, fractional_intensities_polar
 from phasorpy.plot import PhasorPlot, components_histogram
-from phasorpy.phasor import phasor_from_lifetime
+from phasorpy.phasor import phasor_from_lifetime, polar_from_reference_phasor
 import time
 
 fractions = numpy.array(
@@ -13,15 +13,29 @@ components_lifetimes = [8.0, 1.0]
 coordinates = phasor_from_lifetime(
         frequency, components_lifetimes, fractions
     )
+# plot = PhasorPlot(frequency=frequency, title = 'Point lying on the line between components')
+# plot.plot(*coordinates, fmt= 'o-')
+# fractions = fractional_intensities_from_phasor(coordinates[1][0], coordinates[1][1], *phasor_from_lifetime(frequency, components_lifetimes))
+# print (fractions) 
+fractions = fractional_intensities_polar(coordinates[1][0], coordinates[1][1], *phasor_from_lifetime(frequency, components_lifetimes))
+print (fractions) 
+#%%
+fractions = numpy.array(
+    [[1, 0, 0], [0.25 ,0.25, 0.50], [0, 1, 0],[0, 0, 1]]
+)
+frequency = 80.0
+components_lifetimes = [8.0, 3.0, 1.0]
+coordinates = phasor_from_lifetime(
+        frequency, components_lifetimes, fractions
+    )
 #%%
 plot = PhasorPlot(frequency=frequency, title = 'Point lying on the line between components')
-plot.plot(*phasor_from_lifetime(frequency, components_lifetimes), fmt= 'o-')
-plot.plot(coordinates[0][1], coordinates[1][1])
-fractions = fractional_intensities_from_phasor(coordinates[0][1], coordinates[1][1], *phasor_from_lifetime(frequency, components_lifetimes))
+plot.plot(*coordinates, fmt= 'o')
+fractions = fractional_intensities_from_phasor(*coordinates, *phasor_from_lifetime(frequency, components_lifetimes))
 print (fractions) 
 # %%
 real, imag = numpy.random.multivariate_normal(
-    (0.6, 0.35), [[8e-3, 1e-3], [1e-3, 1e-3]], (10, 10)
+    (0.6, 0.35), [[8e-3, 1e-3], [1e-3, 1e-3]], (100, 100)
 ).T
 # plot = PhasorPlot(frequency=frequency, title = 'Point lying on the line between components')
 # plot.plot(*phasor_from_lifetime(frequency, components_lifetimes), fmt= 'o-')
@@ -80,4 +94,32 @@ plt.xlabel('Value')
 plt.ylabel('Frequency')
 plt.grid(True)
 plt.show()
+# %%
+# TIMING IMPLEMENTATIONS
+fractions = numpy.array(
+    [[1, 0], [0.25, 0.75], [0, 1]]
+)
+frequency = 80.0
+components_lifetimes = [8.0, 1.0]
+coordinates = phasor_from_lifetime(
+        frequency, components_lifetimes, fractions
+    )
+real, imag = numpy.random.multivariate_normal(
+    (0.6, 0.35), [[8e-3, 1e-3], [1e-3, 1e-3]], (5000, 5000)
+).T
+new_times = []
+old_times = []
+for i in range(20):
+    start = time.time()
+    fractions = fractional_intensities_from_phasor(real, imag, *phasor_from_lifetime(frequency, components_lifetimes))
+    end = time.time()
+    new_times.append(end-start)
+    start = time.time()
+    fractions = fractional_intensities_from_phasor_old(real, imag, *phasor_from_lifetime(frequency, components_lifetimes))
+    end = time.time()
+    old_times.append(end-start)
+print('AVG NEW: ', numpy.mean(numpy.asarray(new_times)))
+print('AVG old: ', numpy.mean(numpy.asarray(old_times)))
+# %%
+
 # %%
--- src/phasorpy/components.py
+++ src/phasorpy/components.py
@@ -21,6 +21,7 @@ import numpy
 import math
 
 from ._utils import project_phasor_to_line
+from .phasor import polar_from_reference, phasor_to_polar
 
 
 def fractional_intensities_from_phasor(
@@ -61,29 +62,13 @@ def fractional_intensities_from_phasor(
         real, imag, real_components, imaginary_components
     )
     components_coordinates = numpy.array([real_components, imaginary_components])
-    # fraction1 = []
-    # fraction2 = []
-    # projected_real = numpy.atleast_1d(projected_real)
-    # projected_imag = numpy.atleast_1d(projected_imag)
-    # for element in zip(projected_real,projected_imag):
-    #     fractions = numpy.linalg.solve(components_coordinates, element)
-    #     fraction1.append(fractions[0])
-    #     fraction2.append(fractions[1])
-    # fraction1 = numpy.asarray(fraction1)
-    # fraction2 = numpy.asarray(fraction2)
-    # fraction1 = numpy.clip(fraction1, 0, 1)
-    # fraction2 = numpy.clip(fraction2, 0, 1)
-    # return fraction1.reshape(projected_real.shape), fraction2.reshape(projected_real.shape)
     projected_real = numpy.atleast_1d(projected_real)
     projected_imag = numpy.atleast_1d(projected_imag)
-    i = 0
     fractions = []
     for element in zip(projected_real,projected_imag):
         row_fractions = numpy.linalg.solve(components_coordinates, element)
         fractions.append(row_fractions)
-        i += 1
-    print('i=',i)
-    fractions = numpy.stack(fractions, axis=2)
+    fractions = numpy.stack(fractions, axis=-1)
     return fractions
     
 
@@ -141,3 +126,62 @@ def fractional_intensities_from_phasor_old(
     )
     fraction_of_second_component = numpy.clip(fraction_of_second_component, 0 , 1)
     return 1 - fraction_of_second_component, fraction_of_second_component
+
+def fractional_intensities_polar(
+    real: ArrayLike, imag: ArrayLike, real_components: ArrayLike, imaginary_components: ArrayLike, /,
+) -> tuple[NDArray[Any], NDArray[Any]]:
+    """Return fractions of two components from phasor coordinates.
+
+    Parameters
+    ----------
+    real : array_like
+        Real component of phasor coordinates.
+    imag : array_like
+        Imaginary component of phasor coordinates.
+    real_components: array_like
+        Real coordinates of the components.
+    imaginary_components: array_like
+        Imaginary coordinates of the components.
+
+    Returns
+    -------
+    fractions : ndarray
+        Fractions for all components.
+
+    Raises
+    ------
+    ValueError
+        The `signal` has less than three samples along `axis`.
+
+    Examples
+    --------
+    >>> fractional_intensities_from_phasor(
+    ...     [0.6, 0.5, 0.4], [0.4, 0.3, 0.2], [0.2, 0.4], [0.9, 0.3]
+    ... ) # doctest: +NUMBER
+    (...)
+
+    """
+    # projected_real, projected_imag = project_phasor_to_line(
+    #     real, imag, real_components, imaginary_components
+    # )
+    phase, mod = phasor_to_polar(real, imag)
+    first_component = phasor_to_polar(real_components[0], imaginary_components[0])
+    second_component = phasor_to_polar(real_components[1], imaginary_components[1])
+    # phi0_first_component, mod0_first_component = polar_from_reference(phase, mod, *first_component)
+    # phi0_second_component, mod0_second_component = polar_from_reference(phase, mod, *second_component)
+    # Reshape single values to arrays with one column
+    # first_component = numpy.array([first_component[0]]).reshape(-1, 1)
+    # second_component = numpy.array([second_component[0]]).reshape(-1, 1)
+    phase = numpy.array([phase]).reshape(-1, 1)
+    mod = numpy.array([mod]).reshape(-1, 1)
+    # Create coefficient matrix
+    # A = numpy.concatenate((first_component, second_component), axis=1)
+    # Augment coefficient matrix with the equation weight1 + weight2 = 1
+    A = numpy.vstack([first_component, second_component, [1, 1]])
+
+    # Create phase vector
+    b = numpy.concatenate((phase, mod, numpy.array([[1]])))
+
+    # Solve the augmented system of equations
+    fractions = numpy.linalg.solve(A, b)
+    return fractions
