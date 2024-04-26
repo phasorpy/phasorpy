6e45ded11823ba843be2a6e68e8ff191474f2d11
Christoph Gohlke <cgohlke@cgohlke.com>
2024-04-26 13:47:48 -0700

    Print versions in tests and intro tutorial

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+"""Pytest configuration."""
+
+import os
+
+import phasorpy
+from phasorpy import versions
+from phasorpy.utils import number_threads
+
+
+def pytest_report_header(config, start_path, startdir):
+    """Return versions of relevant installed packages."""
+    return '\n'.join(
+        (
+            f'versions: {versions(sep=', ')}',
+            f'number_threads: {number_threads(0)}',
+            f'path: {os.path.dirname(phasorpy.__file__)}',
+        )
+    )
+
+
+collect_ignore = ['data']
