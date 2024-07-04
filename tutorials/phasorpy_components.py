fee0760ed5f5ec4c89ad736387917dea43a33c49
bruno-pannunzio <brunopannunzio@gmail.com>
2024-07-04 16:32:40 -0300

    Update `tutorials/phasorpy_components.py` to hide animation code and other modifications to the visualization of `graphical_component_analysis`.

--- tutorials/phasorpy_components.py
+++ tutorials/phasorpy_components.py
@@ -64,7 +64,7 @@ print(f'Fraction of second component: {fraction_of_second_component:.3f}')
 # phasor coordinates:
 
 real, imag = numpy.random.multivariate_normal(
-    (0.6, 0.35), [[8e-3, 1e-3], [1e-3, 1e-3]], (100, 100)
+    (0.5, 0.33), [[5e-3, 1e-3], [1e-3, 1e-3]], (100, 100)
 ).T
 plot = PhasorPlot(
     frequency=frequency,
@@ -109,20 +109,63 @@ plt.show()
 # Graphical solution for the contribution of multiple components
 # --------------------------------------------------------------
 #
+# The :py:func:`graphical_component_analysis` function performs the graphical
+# analysis for two or three components and returns the number of phasors
+# for each fraction of the components (with respect to other components):
+components_real, components_imag = phasor_from_lifetime(
+    frequency, [1.0, 4.0, 15.0]
+)
+counts, fractions = graphical_component_analysis(
+    real, imag, components_real, components_imag
+)
+fig, ax = plt.subplots()
+plot = PhasorPlot(
+    frequency=frequency,
+    title='Phasor with contribution of three known components',
+    ax=ax,
+)
+plot.hist2d(real, imag, cmap='plasma')
+plot.plot(components_real, components_imag, 'o-', color='blue')
+plot.plot(
+    components_real[[0, -1]], components_imag[[0, -1]], fmt='-', color='blue'
+)
+ax.annotate('A', xy=(0.8, 0.45), fontsize=16, fontweight='bold', color='blue')
+ax.annotate('B', xy=(0.13, 0.43), fontsize=16, fontweight='bold', color='blue')
+ax.annotate(
+    'C', xy=(-0.04, 0.12), fontsize=16, fontweight='bold', color='blue'
+)
+plt.show()
+
+# %%
+# The results can be plotted as histograms for each component pair:
+
+fig, ax = plt.subplots()
+ax.plot(fractions, counts[0], linestyle='-', label='Component A vs B')
+ax.plot(fractions, counts[1], linestyle='-', label='Component A vs C')
+ax.plot(fractions, counts[2], linestyle='-', label='Component B vs C')
+ax.set_xlabel('Fraction of component')
+ax.set_ylabel('Counts')
+ax.set_title('Contribution of multiple components')
+ax.legend()
+plt.show()
+
+# %%
 # The graphical method for resolving the contribution of two or
 # three components (pairwise) to a phasor coordinate is based on
 # the quantification of moving circular cursors along the line
-# between the components, demonstrated in the following animation:
+# between the components, demonstrated in the following animation
+# which is repeated for the other combinations of components:
 
+# sphinx_gallery_start_ignore
 grid_x = numpy.linspace(-0.2, 1, int((1 + 0.2) / 0.001) + 1)
 grid_y = numpy.linspace(-0.2, 0.6, int((0.6 + 0.2) / 0.001) + 1)
 grid_x, grid_y = numpy.meshgrid(grid_x, grid_y)
 cursor_diameter = 0.05
 number_of_steps = 30
 fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(5, 8))
-real_a, imag_a = 0.8, 0.4
-real_b, imag_b = 0.2, 0.4
-real_c, imag_c = 0.042, 0.2
+real_a, imag_a = [components_real[0], components_imag[0]]
+real_b, imag_b = [components_real[1], components_imag[1]]
+real_c, imag_c = [components_real[2], components_imag[2]]
 component_counts = []
 fractions = numpy.asarray(numpy.linspace(0, 1, number_of_steps + 1))
 unit_vector, distance = line_from_components(
@@ -130,10 +173,10 @@ unit_vector, distance = line_from_components(
 )
 cursor_real, cursor_imag = real_b, imag_b
 step_size = distance / number_of_steps
-plot = PhasorPlot(ax=ax1)
+plot = PhasorPlot(frequency=frequency, ax=ax1)
 plot.hist2d(real, imag, cmap='plasma')
-plot.plot([real_a, real_b], [imag_a, imag_b], fmt='o', linestyle='-')
-plot.plot(real_c, imag_c, fmt='o', color='green')
+plot.plot([real_a, real_b], [imag_a, imag_b], fmt='o-', color='blue')
+plot.plot(real_c, imag_c, fmt='o', color='blue')
 plots = []
 for step in range(number_of_steps + 1):
     mask_shape = mask_segment(
@@ -170,38 +213,16 @@ ax2.set_xlim(0, 1)
 ax2.set_title('Fraction of component A (respect to B)')
 ax2.set_xlabel('Fraction of A')
 ax2.set_ylabel('Counts')
+ax1.annotate('A', xy=(0.8, 0.45), fontsize=14, fontweight='bold', color='blue')
 ax1.annotate(
-    'A', xy=(0.84, 0.43), fontsize=16, fontweight='bold', color='blue'
+    'B', xy=(0.13, 0.43), fontsize=14, fontweight='bold', color='blue'
 )
 ax1.annotate(
-    'B', xy=(0.17, 0.43), fontsize=16, fontweight='bold', color='blue'
-)
-ax1.annotate(
-    'C', xy=(0.0, 0.23), fontsize=16, fontweight='bold', color='green'
+    'C', xy=(-0.04, 0.12), fontsize=14, fontweight='bold', color='blue'
 )
 plt.tight_layout()
 plt.show()
-
-# %%
-# The function `graphical_component_analysis` performs this graphical
-# analysis for two or three components and return the number of phasors
-# for each fraction of the components (with respect to other components):
-
-counts, fractions = graphical_component_analysis(
-    real, imag, [real_a, real_b, real_c], [imag_a, imag_b, imag_c]
-)
-
-# %%
-# The results can be plotted as histograms for each component pair:
-
-fig, ax = plt.subplots()
-ax.plot(fractions, counts[0], linestyle='-', label='Component A vs B')
-ax.plot(fractions, counts[1], linestyle='-', label='Component A vs C')
-ax.plot(fractions, counts[2], linestyle='-', label='Component B vs C')
-ax.set_xlabel('Fraction of component')
-ax.set_ylabel('Counts')
-ax.legend()
-plt.show()
+# sphinx_gallery_end_ignore
 
 # %%
 # sphinx_gallery_thumbnail_number = 2
