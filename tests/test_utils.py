c410b4a3ccc792f5856dd2205cb37d09c96c108b
Christoph Gohlke <cgohlke@cgohlke.com>
2024-10-04 13:26:21 -0700

    Add Anscombe transformation functions (#139)

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,16 @@
 
 import os
 
-from phasorpy.utils import number_threads
+import numpy
+import pytest
+
+from phasorpy.utils import (
+    anscombe_transformation,
+    anscombe_transformation_inverse,
+    number_threads,
+)
+
+numpy.random.seed(42)
 
 
 def test_number_threads():
@@ -23,6 +32,27 @@ def test_number_threads():
         assert number_threads(0) == 4
         assert number_threads(6) == 6
         del os.environ['PHASORPY_NUM_THREADS']
+
+
+@pytest.mark.parametrize('dtype', ['float32', 'uint16'])
+def test_anscombe_transformation(dtype):
+    """Test anscombe_transformation and inverse functions."""
+    x = numpy.random.poisson(10, 100000).astype(dtype)
+    if dtype == 'float32':
+        x[0] = numpy.nan
+    z = anscombe_transformation(x)
+    numpy.testing.assert_allclose(numpy.std(z[1:]), 1.0, atol=0.01)
+
+    x2 = anscombe_transformation_inverse(z)
+    numpy.testing.assert_allclose(x2[1:], x[1:], atol=0.01)
+
+    x3 = anscombe_transformation_inverse(z, approx=True)
+    numpy.testing.assert_allclose(x3[1:], x[1:], atol=1.0)
+
+    if dtype == 'float32':
+        assert numpy.isnan(z[0])
+        assert numpy.isnan(x2[0])
+        assert numpy.isnan(x3[0])
 
 
 # mypy: allow-untyped-defs, allow-untyped-calls
