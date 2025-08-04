f9b2bf8c3c3f72647b97e1310e97e39c0a03052f
Christoph Gohlke <cgohlke@cgohlke.com>
2025-08-04 16:51:05 -0700

    Rename build_manylinux scripts

--- tools/build_manylinux.cmd
+++ tools/build_manylinux.cmd
@@ -1,11 +1,11 @@
-:: Build PhasorPy manylinux2014 wheels on Windows using Docker
+:: Build PhasorPy manylinux wheels on Windows using Docker
 
 setlocal
 set PATH=C:\Windows;C:\Windows\System32;C:\Program Files\Docker\Docker\resources\bin
 set CIBW_ARCHS_LINUX=auto
-set CIBW_SKIP=pp* cp37* cp38* cp39* cp310* *musllinux*
-:: set CIBW_TEST_SKIP=*
-set CIBW_TEST_COMMAND=python -m pytest {project}/tests
+set CIBW_SKIP=cp38* cp39* cp310* *musllinux*
+set CIBW_TEST_SKIP=cp314*
+set CIBW_TEST_COMMAND=pytest {project}/tests
 set CIBW_BUILD_VERBOSITY=3
 
 docker version
--- tools/build_manylinux.sh
+++ tools/build_manylinux.sh
@@ -1,8 +1,8 @@
-# Build PhasorPy manylinux2014 wheels on Linux or macOS using Docker
+# Build PhasorPy manylinux wheels on Linux or macOS using Docker
 
 export CIBW_ARCHS_LINUX=auto
-export CIBW_SKIP="pp* cp37* cp38* cp39* cp310* *musllinux*"
-# export CIBW_TEST_SKIP="*"
+export CIBW_SKIP="cp38* cp39* cp310* *musllinux*"
+export CIBW_TEST_SKIP=cp314*
 export CIBW_TEST_COMMAND="pytest {project}/tests"
 export CIBW_BUILD_VERBOSITY=3
 
