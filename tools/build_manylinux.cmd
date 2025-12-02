411f11afe429e0c5302ae92692d5e9626f0d8833
Christoph Gohlke <cgohlke@cgohlke.com>
2025-12-01 20:56:54 -0800

    Use dependency groups instead of requirement files

--- tools/build_manylinux.cmd
+++ tools/build_manylinux.cmd
@@ -1,10 +1,10 @@
-:: Build PhasorPy manylinux wheels on Windows using Docker
+:: Build PhasorPy manylinux ABI3 wheels on Windows using Docker
 
 setlocal
 set PATH=C:\Windows;C:\Windows\System32;C:\Program Files\Docker\Docker\resources\bin
 set CIBW_ARCHS_LINUX=auto
-set CIBW_SKIP=cp38* cp39* cp310* *musllinux*
-set CIBW_TEST_SKIP=cp314*
+set CIBW_SKIP=cp38* cp39* cp310* cp311* cp313* cp314-* *musllinux*
+set CIBW_TEST_SKIP=cp314t*
 set CIBW_TEST_COMMAND=pytest {project}/tests
 set CIBW_BUILD_VERBOSITY=3
 
